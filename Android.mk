9109d285e53751a4d7eaa19506def9fde7b39a63
Roman Birg <romanbirg@gmail.com>
2012-05-11 11:06:17 -0500

    tf201: revert changes to Android.mk to fix build
    Change-Id: I6db926cb84733c22e5bbfff2619b82a0a8c1b9c3

--- Android.mk
+++ Android.mk
@@ -1,9 +1,1 @@
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),tf201)
-
-ifneq ($(TARGET_SIMULATOR),true)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
-
-endif
+LOCAL_PATH := $(my-dir)
